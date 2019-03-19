965daff0fb162b9fdaf9f3e62906a54fbf9bbe0a
thaani <38487289+thaani@users.noreply.github.com>
2019-03-19 08:48:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.videoplayer;
+
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+public class MainActivity extends AppCompatActivity {
+    MediaController mediaController;
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView =(VideoView)findViewById(R.id.videoView1);
+        //requestStoragePermission();
+        perm();
+
+       //
+
+
+
+
+
+    }
+
+
+
+
+
+    public void perm(){
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        mediaController= new MediaController(MainActivity.this);
+                        mediaController.setAnchorView(videoView);
+
+                        //specify the location of media file
+                        String path = "android.resource://" + getPackageName() + "/" + R.raw.video;
+
+                        videoView.setMediaController(mediaController);
+                        videoView.setVideoURI(Uri.parse(path));
+
+                        videoView.requestFocus();
+                        videoView.start();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        // check for permanent denial of permission
+                        if (response.isPermanentlyDenied()) {
+                            // navigate user to app settings
+                        }
+                    }
+
+                     @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
---
+++ app/src/main/java/com/example/videoplayer/splash.java
@@ -0,0 +1,29 @@
+package com.example.videoplayer;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash2);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
